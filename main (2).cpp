5ba4897aac5db222b62e8ee32f295e3ddc1efdcf
LilEngineering <144496804+LilEngineering@users.noreply.github.com>
2023-09-08 18:44:29 -0400

    Update main (2).cpp

--- main (2).cpp
+++ main (2).cpp
@@ -67,12 +67,6 @@ int main(){
                 
             }
         }
- echo "# Birthday-Conditionals-Code-Practice-" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/LilEngineering/Birthday-Conditionals-Code-Practice-.git
-git push -u origin main       
+    
 }
 
